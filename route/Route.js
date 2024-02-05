2797b99b71d8a91ac8892349655692d2e4a7c8dd
poluruhemu <poluruhemu@gmail.com>
2024-02-05 13:22:23 +0530

    intial commit

---
+++ route/Route.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const router = express.Router();
+const fs = require('fs');
+
+const accountRoutes = require('./account.js') // import account route
+router.use(accountRoutes) // use account route
+
+module.exports = router;
